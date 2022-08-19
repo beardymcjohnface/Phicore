efe6687c8a49f56f7eb9252b6b253c41ec566c8b
Michael Roach <beardymcjohnface@gmail.com>
2022-08-19 14:22:22 +0930

    fixes

--- strand_swaps/strand_swaps/workflow/scripts/combineConsecTsv.py
+++ strand_swaps/strand_swaps/workflow/scripts/combineConsecTsv.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 
-def parseTsv(file=None, category=None):
-    sample = file.replace('.tsv','')
+def parseTsv(sample=None, file=None, category=None):
     with open(file, 'r') as f:
         for line in f:
             l = line.strip()
@@ -10,12 +9,8 @@ def parseTsv(file=None, category=None):
 
 
 with open(snakemake.output, 'w') as out:
-    for file in snakemake.input.strand:
-        for line in parseTsv(file=file, category='strand'):
-            out.write(line)
-    for file in snakemake.input.frame:
-        for line in parseTsv(file=file, category='frame'):
-            out.write(line)
-    for file in snakemake.input.overlap:
-        for line in parseTsv(file=file, category='overlap'):
-            out.write(line)
+    for sample in snakemake.params.samples:
+        for suffix in snakemake.params.suffixes.keys():
+            file = os.path.join(snakemake.params.dir, sample + snakemake.params.suffixes[suffix])
+            for line in parseTsv(file=file, category=suffix, sample=sample):
+                out.write(line)
